0e49a446440242e6adf9972c856bd82a6823f9df
Kunal Bhandari <kunal_nitk@yahoo.com>
2020-11-22 12:13:53 -0600

    Added crontab and upgrade scrips for ubuntu

---
+++ ubuntu_upgrade.sh
@@ -0,0 +1,8 @@
+# https://askubuntu.com/questions/196768/how-to-install-updates-via-command-line
+sudo -s -- <<EOF
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+apt-get autoclean -y
+EOF
